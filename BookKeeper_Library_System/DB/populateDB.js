96d26a37a4758d225f84455720a84a78f47ab99a
dmamye <dmamye>
2020-02-05 14:14:05 -0400

    Added multiple statement functionality to database populate file

--- BookKeeper_Library_System/DB/populateDB.js
+++ BookKeeper_Library_System/DB/populateDB.js
@@ -4,15 +4,26 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "BookKeeper_System"
+    database: "BookKeeper_System",
+    multipleStatements: true
+
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE book_information (id INT AUTO_INCREMENT PRIMARY KEY, bookTitle VARCHAR(50), genre INT, publicationDate DATE, description VARCHAR(200))";
+    var sql = "CREATE TABLE book_information (" +
+        "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+        "bookTitle VARCHAR(50) NOT NULL, " +
+        "genre INT NOT NULL, " +
+        "publicationDate DATE, " +
+        "description VARCHAR(200)); " +
+        "CREATE TABLE test (" +
+        "test INT NOT NULL);";
+    console.log(sql);
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Table created");
+        console.log(sql);
     });
 });
