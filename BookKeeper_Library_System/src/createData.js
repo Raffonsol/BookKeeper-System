806cbdffa483c1292e6eb1d6a29d4fe5eaa2e8f6
Ricardo <ricardo_vieira32@outlook.com>
2020-02-09 11:31:10 -0400

    feat(src): form creation code

---
+++ BookKeeper_Library_System/src/createData.js
@@ -0,0 +1,27 @@
+
+var dataType = null;
+
+/**
+ * Enables dropdown selections based on user permissions
+ */
+$(() => {
+    setTimeout( () => {
+        var forbiddenElements = document.getElementsByClassName(activeRole);
+        for (let i = 0; i < forbiddenElements.length ; i++) {
+            forbiddenElements[i].style.display = 'inline';
+        }
+    }, 10);
+});
+
+/**
+ * loads the selected add form
+ * to post entries to the database
+ * @param form
+ */
+function showForm(form) {
+    if (form === 'null') {
+        return;
+    }
+    dataType = form;
+    $("#form").load('components/' + form + 'Form.html');
+}
