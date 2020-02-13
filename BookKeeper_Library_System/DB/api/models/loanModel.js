c3da4767a6742b58c8962f63c6c12fe4796890ac
Ricardo <ricardo_vieira32@outlook.com>
2020-02-13 19:48:29 -0400

    feat(models): all the models there can be

---
+++ BookKeeper_Library_System/DB/api/models/loanModel.js
@@ -0,0 +1,93 @@
+'loan strict';
+var sql = require('../db.js');
+
+var Loan = function (loan) {
+    this.transactionId = null;
+    this.bookId = loan.bookId;
+    this.memberAccount = loan.memberAccount;
+    this.employeeId = loan.employeeId;
+
+    var today = new Date();
+    this.dueDate = today.setDate(today.getDate() + loan.loanDays);
+};
+
+Loan.createLoan = function (newLoan, result) {
+    var transactionID;
+    console.log('newLoan:', newLoan);
+    var transaction = {
+        employeeId: newLoan.employeeId,
+        memberId: newLoan.memberAccount,
+        date: new Date().getDate(),
+        time: new Date().getTime()
+    };
+
+    sql.query("INSERT INTO transaction set ?", transaction, function (err, res) {
+
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+        } else {
+            console.log('transaction id:', res.insertId);
+            result(null, res);
+
+            newLoan.transactionId = res.insertId;
+
+            sql.query("INSERT INTO memberaccount set ?", newLoan, function (err, res) {
+
+                if (err) {
+                    console.log("error: ", err);
+                    result(err, null);
+                } else {
+                    console.log('loan id:', res.insertId);
+                    result(null, res);
+                }
+            });
+        }
+    });
+};
+Loan.getLoanById = function (loanId, result) {
+    sql.query("SELECT * from memberaccount where id = ? ", loanId, function (err, res) {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+        } else {
+            result(null, res);
+        }
+    });
+};
+Loan.getAllLoans = function (result) {
+    sql.query("Select * from memberaccount", function (err, res) {
+
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        } else {
+            console.log('loans : ', res);
+            result(null, res);
+        }
+    });
+};
+Loan.updateById = function (id, loan, result) {
+    sql.query("UPDATE memberaccount SET name = ? WHERE id = ?", [loan.name, id], function (err, res) {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        } else {
+            result(null, res);
+        }
+    });
+};
+Loan.remove = function (id, result) {
+    sql.query("DELETE FROM memberaccount WHERE id = ?", [id], function (err, res) {
+
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        } else {
+
+            result(null, res);
+        }
+    });
+};
+
+module.exports = Loan;
---
+++ BookKeeper_Library_System/DB/api/models/transactionModel.js
@@ -0,0 +1,32 @@
+'transaction strict';
+var sql = require('../db.js');
+
+var Transaction = function (transaction) {
+
+};
+
+
+Transaction.getTransactionById = function (transactionId, result) {
+    sql.query("SELECT * from memberaccount where id = ? ", transactionId, function (err, res) {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+        } else {
+            result(null, res);
+        }
+    });
+};
+Transaction.getAllTransactions = function (result) {
+    sql.query("Select * from memberaccount", function (err, res) {
+
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        } else {
+            console.log('transactions : ', res);
+            result(null, res);
+        }
+    });
+};
+
+module.exports = Transaction;
