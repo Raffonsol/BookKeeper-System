e579b82ba87a3f17f48ddde94517dd45e7b61e78
Ricardo <ricardo_vieira32@outlook.com>
2020-02-12 20:50:58 -0400

    feat: create server

--- BookKeeper_Library_System/DB/server.js
+++ BookKeeper_Library_System/DB/server.js
@@ -1,7 +1,35 @@
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 
+app.use(cors());
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root"
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE IF NOT EXISTS BookKeeper_System", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });
+});
+
 app.listen(port);
 
 console.log('Library API server started on: ' + port);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/libraryRoutes'); //importing route
+routes(app); //register the route
