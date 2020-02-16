b4392bb4ed72d63b0675f3ebe9ed412bd6092633
Ricardo <ricardo_vieira32@outlook.com>
2020-02-16 15:44:37 -0400

    Auto stash before merge of "index" and "origin/index"

--- BookKeeper_Library_System/src/router.js
+++ BookKeeper_Library_System/src/router.js
@@ -9,7 +9,10 @@ function navigate(pageURL, users = []) {
         $("#content").load(pageURL);
         setTimeout( ()=> {
             loginSection.enableAccessiblePages();
+<<<<<<< Updated upstream
             if (pageURL.includes('bookSearch'))viewBooks();
+=======
+>>>>>>> Stashed changes
 
             if (pageURL.includes('home')) home.load()
         }, 200);
